6059420094a53de5f09a88fad19d70179d85e97e
mrhuangyuhui <mrhuangyuhui@gmail.com>
2017-08-16 10:26:28 +0800

    * 重构代码

--- stock_manager.py
+++ stock_manager.py
@@ -6,23 +6,42 @@ import pprint
 wb = openpyxl.load_workbook(sys.argv[1])
 sheet = wb.get_sheet_by_name(unicode('上架0815', "utf-8"))
 
+# 字段名称
+PRD_GIFT_ID = 'gift_id'
+PRD_CODE = 'prd_code'
+PRD_NAME = 'prd_name'
+PRD_POINT = 'origin_point'
+PRD_TYPE = 'type'
+PRD_EX_TIMES = 'ex_times'
+PRD_MONTH_EXCHANGE = 'month_exchange'
+PRD_START_TIME = 'start_time'
+PRD_END_TIME = 'end_time'
+PRD_EX_TYPE = 'exchange_type'
+PRD_STOCK_COUNT = 'total_count'
+PRD_MODULE = 'module'
+PRD_SEQ_NO = 'seq_no'
+PRD_STORE = 'store'
+PRD_CARD = 'card_id'
+PRD_DEL = 'is_del'
+
+# xlsx字母列对应的字段名
 PRD_COLUMN_NAME_DICT = {}
-PRD_COLUMN_NAME_DICT['D'] = 'gift_id'
-PRD_COLUMN_NAME_DICT['E'] = 'prd_code'
-PRD_COLUMN_NAME_DICT['F'] = 'prd_name'
-PRD_COLUMN_NAME_DICT['G'] = 'origin_point'
-PRD_COLUMN_NAME_DICT['I'] = 'type'
-PRD_COLUMN_NAME_DICT['J'] = 'ex_times'
-PRD_COLUMN_NAME_DICT['K'] = 'month_exchange'
-PRD_COLUMN_NAME_DICT['L'] = 'start_time'
-PRD_COLUMN_NAME_DICT['M'] = 'end_time'
-PRD_COLUMN_NAME_DICT['N'] = 'exchange_type'
-PRD_COLUMN_NAME_DICT['O'] = 'total_count'
-PRD_COLUMN_NAME_DICT['Q'] = 'module'
-PRD_COLUMN_NAME_DICT['R'] = 'seq_no'
-PRD_COLUMN_NAME_DICT['S'] = 'store'
-PRD_COLUMN_NAME_DICT['T'] = 'card_id'
-PRD_COLUMN_NAME_DICT['U'] = 'is_del'
+PRD_COLUMN_NAME_DICT['D'] = PRD_GIFT_ID
+PRD_COLUMN_NAME_DICT['E'] = PRD_CODE
+PRD_COLUMN_NAME_DICT['F'] = PRD_NAME
+PRD_COLUMN_NAME_DICT['G'] = PRD_POINT
+PRD_COLUMN_NAME_DICT['I'] = PRD_TYPE
+PRD_COLUMN_NAME_DICT['J'] = PRD_EX_TIMES
+PRD_COLUMN_NAME_DICT['K'] = PRD_MONTH_EXCHANGE
+PRD_COLUMN_NAME_DICT['L'] = PRD_START_TIME
+PRD_COLUMN_NAME_DICT['M'] = PRD_END_TIME
+PRD_COLUMN_NAME_DICT['N'] = PRD_MONTH_EXCHANGE
+PRD_COLUMN_NAME_DICT['O'] = PRD_STOCK_COUNT
+PRD_COLUMN_NAME_DICT['Q'] = PRD_MODULE
+PRD_COLUMN_NAME_DICT['R'] = PRD_SEQ_NO
+PRD_COLUMN_NAME_DICT['S'] = PRD_STORE
+PRD_COLUMN_NAME_DICT['T'] = PRD_CARD
+PRD_COLUMN_NAME_DICT['U'] = PRD_DEL
 
 # 将某一行的数据构建成一个字典
 def buildRowDict(worksheet, row_index):
