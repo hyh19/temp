9ffb1634add4d8ec65363910d303f7f1f72195e6
mrhuangyuhui <mrhuangyuhui@gmail.com>
2017-08-17 10:33:40 +0800

    + 字符串处理函数

--- utils_str.py
+++ utils_str.py
@@ -1,5 +1,6 @@
 # -*- coding: UTF-8 -*-
 import string
+from datetime import datetime
 
 # 移除字符串左右两侧的空白符
 def strippedString(str):
@@ -7,3 +8,12 @@ def strippedString(str):
 
 str = "  hello world  "
 print strippedString(str)
+
+# 字符串转化为整数
+num = "12345"
+print type(int(num))
+
+# 字符串转化为日期时间
+dt = datetime.strptime("2017-08-15 10:00:15", "%Y-%m-%d %H:%M:%S")
+print type(dt)
+print dt
